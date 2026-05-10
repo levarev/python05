4d4cb6c24047af93e62735e80aa9376b4b079c75
levarev <levonarev2@gmail.com>
2026-05-10 14:12:54 +0400

    Add files via upload

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -6,7 +6,7 @@ class DataProcessor(ABC):
     def __init__(self) -> None:
         self._ret: list[tuple[int, str]] = []
         self._index: int = 0
-        self._not: self._index #outputum poxenq not - 1
+        self._not: int = 0
 
     @abstractmethod
     def validate(self, data: Any) -> bool:
@@ -19,14 +19,13 @@ class DataProcessor(ABC):
     def output(self) -> tuple[int, str]:
         if not self._ret:
             raise TypeError()
-        s
         return self._ret.pop(0)
-
-    def ret_not(self) -> int:
-        return self._not
 
     def ret_index(self) -> int:
         return self._index
+
+    def ret_ret(self) -> int:
+        return len(self._ret)
 
 
 class NumericProcessor(DataProcessor):
@@ -132,8 +131,6 @@ class DataStream():
                 if proc.validate(x) is True:
                     proc.ingest(x)
                     check = True
-                else:
-                    proc._not += 1
             if check is False:
                 print(f"DataStream error - Can't process element in stream: {x}")
             check = False
@@ -144,13 +141,14 @@ class DataStream():
             print("No processor found, no data")
         else:
             for x in self._procs:
-                print(f"{x.__class__.__name__}: total {x.ret_index()} items processed, remaining {x.ret_not()} on processor")
+                print(f"{x.__class__.__name__}: total {x.ret_index()} items processed, remaining {x.ret_ret()} on processor")
 
 
 if __name__ == "__main__":
     a = DataStream()
     num = NumericProcessor()
     text = TextProcessor()
+    log = LogProcessor()
     print('=== Code Nexus - Data Stream ===')
     a.print_processors_stats()
     print("\nRegistering Numeric Processor\n")
@@ -159,4 +157,17 @@ if __name__ == "__main__":
     print(f"Send first batch of data {lst}")
     a.register_processor(num)
     a.process_stream(lst)
+    a.print_processors_stats()
+    print("\nRegistering other data processors")
+    a.register_processor(text)
+    a.register_processor(log)
+    a.process_stream(lst)
+    a.print_processors_stats()
+    print("\nConsume some elements from the data processors: Numeric 3, Text 2, Log 1")
+    for _ in range(3):
+        num.output()
+    for _ in range(2):
+        text.output()
+    for _ in range(1):
+        log.output()
     a.print_processors_stats()
